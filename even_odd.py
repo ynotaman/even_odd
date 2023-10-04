dfadeef0e4b856842c8e54fe8a5b2001e181a1b6
MOHAMMAD AMAN KHAN <146350867+ynotaman@users.noreply.github.com>
2023-10-04 23:16:09 +0530

    Create even_odd.py

---
+++ even_odd.py
@@ -0,0 +1,17 @@
+a=int(input("eneter the value of a : "))
+print (a)
+if(a%2==0):
+    print("the given number is even ")
+else:
+    print("the given number is odd ")
+    
+a=input("enter the number a :")
+print(a)
+b= input ("enter the number b : ")
+print(b)
+if (a==b):
+    print("a is equal to b")
+elif (a>b):
+    print("a is greater then b")
+else:
+    print("a is lesser than b")
